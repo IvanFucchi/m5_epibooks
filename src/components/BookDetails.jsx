0714f62f1b90991734e5fb286cfe92a6be3af650
IvanFucchi <ivanfucchi@gmail.com>
2025-04-04 23:56:57 +0200

    book_detail_animation

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,21 +1,40 @@
+import React, { useState } from 'react';
 import { Container, Col, Row, Image } from "react-bootstrap";
+import { motion } from 'framer-motion';
+
 
 const BookDetails = ({ book }) => {
+  const [hovered, setHovered] = useState(false);
 
   return (
     <>
       <Container className="my-4">
         <Row className="align-items-start d-flex">
           <Col xs={12} md={8} lg={9} className="h-90" >
+          <motion.div
+            onHoverStart={() => setHovered(true)}
+            onHoverEnd={() => setHovered(false)}
+            initial={{opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1}}
+            whileHover={{ scale: 1.05}}
+            transition={{ duration: 1 }}
+            style={{
+              display: 'inline-block',
+              borderRadius: '12px',
+              boxShadow: hovered
+                ? '0 12px 24px rgba(1, 1, 1, 1)'
+                : '0 4px 8px rgba(0, 0, 0, 0.2)',
+            }}
+          >
             <Image 
             src={book.img} 
             alt={book.title} 
             fluid
             rounded
-            thumbnail
-            className="shadow mb-5"
-            style={{ maxHeight: '700px', objectFit: 'contain' }} 
+            className="shadow "
+            style={{ maxHeight: '700px', objectFit: 'cover', borderRadius: '12px' }} 
             />
+            </motion.div>
           </Col>
           <Col xs={12} md={4} lg={3}>
           <h2>{book.title}</h2>
